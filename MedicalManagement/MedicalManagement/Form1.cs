c490ba4308fbfdc7d85fbb89cb2b52eff6023db2
Đức Huy <95405766+JRHuy@users.noreply.github.com>
2022-01-01 23:11:17 +0700

    Edit import/export form

---
+++ MedicalManagement/MedicalManagement/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedicalManagement
+{
+    public partial class Form1 : Form
+    {
+        MEDICALCOMPANYEntities db = new MEDICALCOMPANYEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool adminCheck = db.userLogins.Any(x=>x.username==usertxt.Text && x.userPass==passtxt.Text && x.userType=="admin");
+            bool userCheck = db.userLogins.Any(x => x.username == usertxt.Text && x.userPass == passtxt.Text && x.userType == "user");
+
+            if (adminCheck)
+            {
+                AdminForm ad = new AdminForm();
+                this.Hide();
+                ad.Show();
+                MessageBox.Show("Login Successfully");
+            }else if (userCheck)
+            {
+                Import uf = new Import();
+                this.Hide();
+                uf.Show();
+                MessageBox.Show("Login Successfully");
+            }
+            else
+            {
+                MessageBox.Show("Username or Password incorrect");
+            }
+
+        }
+    }
+}
